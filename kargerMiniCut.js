e374cf0eefa3e8a5015bb823b26d689e0180aecf
sh20022002 <shmuel.tor@gmail.com>
2024-04-28 23:25:02 +0300

    initial commit

---
+++ kargerMiniCut.js
@@ -0,0 +1,15 @@
+const fs = require('fs');
+
+fs.readFile('javaScript\\graphCut\\graph.txt', 'utf-8', (err, data) => {
+    if(err){
+        console.error(err);
+        return;
+    }
+
+});
+
+function minCut(graph){
+    while(v > 2){
+        
+    }
+}
