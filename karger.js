d50a312dc27bd1d497ca61b007b8371e63aac542
sh20022002 <shmuel.tor@gmail.com>
2024-05-06 01:11:35 +0300

    working well

--- karger.js
+++ karger.js
@@ -28,15 +28,39 @@ function randomizedChoise(graph){
     let choise = randomize(1, graph[choise1].length - 1);
     let choise2 = getIndex(graph[choise1][choise]);
     // calls the contract function with the graph and the indexes of the two vertcies to contract
-    return [choise1, choise2];
+    if(choise1 === 'undefined' || choise2 === 'undefined'){
+        console.log('undefined');
+    }else{
+        return [choise1, choise2];
+    }
 }
 function contract(graph, c1, c2){
     // contracts two vertcies from the graph ranom choise 1 & 2(c1 , c2)
+    function combain(val1, val2){
+        // chack if val is an array if it is push each element to temp
+        // else push it as a value
+        let temp = new Array();
+        if(typeof val1 === 'object'){
+            for( num of val1){
+                temp.push(num);
+            }
+        }else{
+            temp.push(val1);
+        }
+        if(typeof val2 === 'object'){
+            for(let num of val2){
+                temp.push(num);
+            }
+        }else{
+            temp.push(val2);
+            }
+        return temp;
+        }
     
     const len = Math.max(graph[c1].length, graph[c2].length); // the longest vertcie
     var temp = [];
     // sets the first vertcie as a super vertice with the names of all previous contracted vertcies
-    temp.push([...graph[c1][0], ...graph[c2][0]]);
+    temp.push(combain(graph[c1][0],graph[c2][0]));
     for(let edge = 1; edge < len; edge++){
         // iterates from 1 to longest vertcies length and push only the edges the should not contract
         
@@ -54,13 +78,17 @@ function contract(graph, c1, c2){
     // sets one of the vertcies in the graph the contracted to its updated values deletes the other vertcie and returns to recursion 
     // with the updated graph
     if(typeof temp[0] === 'object'){
-        for(let edge = 1; edge < temp.length; edge++){
-            if(temp[0].includes(temp[edge])){
-                temp.splice(edge, 1);
+        var newArray = temp.filter((item, index) => {
+            if (index === 0) {
+                // Keep the first inner array intact
+                return true;
+            } else {
+                // Remove elements from the main array that are present in the first inner array
+                return !temp[0].includes(item);
             }
-        }
+        });
     }
-    graph[c1] = temp;
+    graph[c1] = newArray;
     graph.splice(c2, 1);
     return graph;
 
@@ -86,7 +114,7 @@ fs.readFile('graphCut\\graph.txt', 'utf-8', (err, data) => {
     const graph = [];
 // splits to lines and than to elemnts with spaces between
     for(let line of data.split('\n')){
-        let temp = line.split(/\s+/);
+        let temp = line.split(/\s+/).map(Number);
         temp.pop();
         graph.push(temp);
         
@@ -94,6 +122,7 @@ fs.readFile('graphCut\\graph.txt', 'utf-8', (err, data) => {
     // console.log(typeof graph);
     const ans = karger(graph);
     console.log(ans[0].length -1);
+    console.log(ans[1].length -1);
     
 
 });
